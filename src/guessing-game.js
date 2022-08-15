928f0a560e1c53f3483bd070a0a11bd2088274e0
EVGENIY <parukevich@gmail.com>
2022-08-15 20:58:47 +0300

    feat: inplemenе methods

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.number;
+        this.start;
+        this.end;
+        this.middle;
+    }
 
     setRange(min, max) {
-
+        if (max >= min) {
+            this.start = min;
+            this.end = max;
+        }
     }
 
     guess() {
-
+        return this.middle = Math.ceil((this.start + this.end) / 2);
     }
 
     lower() {
-
+        this.end = this.middle;
     }
 
     greater() {
-
+        this.start = this.middle;
     }
 }
 
